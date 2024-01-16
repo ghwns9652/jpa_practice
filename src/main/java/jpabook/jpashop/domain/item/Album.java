65783c5aee9cc1bc0242eea401657cfa472b402f
ghwns9652 <ghwns9652@dgist.ac.kr>
2024-01-17 07:47:57 +0900

    Added classes

--- src/main/java/jpabook/jpashop/domain/item/Album.java
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -1,7 +1,14 @@
 package jpabook.jpashop.domain.item;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-public class Album {
+@DiscriminatorValue("A")
+@Getter @Setter
+public class Album extends Item {
+    private String artist;
+    private String etc;
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,13 +1,12 @@
 package jpabook.jpashop.domain.item;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
 @Getter @Setter
 public class Item {
     @Id
